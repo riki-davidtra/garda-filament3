2ce8115a549d18928f65e5b8fe0db488be215451
Riki Davidtra <rikidavidtra.2310@gmail.com>
2025-09-09 13:48:44 +0700

    update

--- app/Filament/Resources/TemplatDokumenResource.php
+++ app/Filament/Resources/TemplatDokumenResource.php
@@ -110,13 +110,18 @@ class TemplatDokumenResource extends Resource
                 Tables\Actions\Action::make('unduh')
                     ->label('Unduh')
                     ->button()
+                    ->color('info')
                     ->icon('heroicon-o-arrow-down-tray')
                     ->url(fn($record) => route('template.unduh', $record->id))
                     ->openUrlInNewTab()
                     ->visible(fn($record) => filled($record?->path) && Storage::disk('public')->exists($record->path)),
 
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make()
+                    ->button(),
+
+                Tables\Actions\EditAction::make()
+                    ->button()
+                    ->color('warning'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
