e9b100329e97cf1d08936ddb60f6afe924f70f84
Riki Davidtra <rikidavidtra.2310@gmail.com>
2025-09-17 23:00:42 +0700

    update

--- routes/console.php
+++ routes/console.php
@@ -10,5 +10,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 // app(Schedule::class)->command('notifikasi-jadwal-dokumen')->dailyAt('00:00');
-// app(Schedule::class)->job(new JadwalDokumenJob)->everyMinute();
+app(Schedule::class)->job(new JadwalDokumenJob)->everyMinute();
 app(Schedule::class)->job(new JadwalDokumenJob)->dailyAt('08:00');
